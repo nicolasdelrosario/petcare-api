1c111d6c015078486ec86dca1d79f22630b62e28
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-03-04 18:43:39 -0500

    feat: :sparkles: move create app to its own file and add the new middlewares

---
+++ src/lib/create-app.ts
@@ -0,0 +1,28 @@
+import { OpenAPIHono } from "@hono/zod-openapi";
+import { logger } from "hono/logger";
+
+import type { Env } from "@/lib/types";
+
+import { defaultHook } from "@/middleware/default-hook";
+import { notFound } from "@/middleware/not-found";
+import { onError } from "@/middleware/on-error";
+import { serveEmojiFavicon } from "@/middleware/serve-emoji-favicon";
+
+export function createRouter() {
+	return new OpenAPIHono<Env>({
+		strict: false,
+		defaultHook,
+	});
+}
+
+export function createApp() {
+	const app = createRouter();
+
+	app.use(serveEmojiFavicon("🐇"));
+	app.use(logger());
+
+	app.notFound(notFound);
+	app.onError(onError);
+
+	return app;
+}
