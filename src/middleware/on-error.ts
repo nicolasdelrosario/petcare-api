75fcf00285f0e0141e1cb20e75e3a4446d6b14e9
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-03-04 18:42:13 -0500

    feat: :sparkles: add custom on error handler

---
+++ src/middleware/on-error.ts
@@ -0,0 +1,23 @@
+import type { ErrorHandler } from "hono";
+import type { ContentfulStatusCode, StatusCode } from "hono/utils/http-status";
+
+import { INTERNAL_SERVER_ERROR, OK } from "@/constants/http-status-codes";
+
+export const onError: ErrorHandler = (err, c) => {
+	const currentStatus =
+		"status" in err ? (err.status as StatusCode) : c.res.status;
+
+	const statusCode =
+		currentStatus !== OK ? currentStatus : INTERNAL_SERVER_ERROR;
+
+	// only show stack trace in development
+	const isProd = c.env && c.env.NODE_ENV === "development";
+
+	return c.json(
+		{
+			message: err.message,
+			stack: isProd ? undefined : err.stack,
+		},
+		statusCode as ContentfulStatusCode,
+	);
+};
