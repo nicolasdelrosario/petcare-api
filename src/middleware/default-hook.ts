238bcf60dcd08d007f9952358fe6175f9eec7185
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-03-04 18:40:00 -0500

    feat: :sparkles: add default hook for handling validation errors

---
+++ src/middleware/default-hook.ts
@@ -0,0 +1,16 @@
+import type { Hook } from "@hono/zod-openapi";
+
+import { UNPROCESSABLE_ENTITY } from "@/constants/http-status-codes";
+
+// biome-ignore lint/suspicious/noExplicitAny: allows flexibility by making the hook reusable for any validation schema.
+export const defaultHook: Hook<any, any, any, any> = (result, c) => {
+	if (!result.success) {
+		return c.json(
+			{
+				success: result.success,
+				error: result.error,
+			},
+			UNPROCESSABLE_ENTITY,
+		);
+	}
+};
