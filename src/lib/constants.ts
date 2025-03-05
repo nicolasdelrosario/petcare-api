8e4262b7e5abff3933073917a09abe6ac1108350
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-03-04 20:07:48 -0500

    feat: :sparkles: add common schemas

---
+++ src/lib/constants.ts
@@ -0,0 +1,28 @@
+import * as HttpStatusPhrases from "@/constants/http-status-phrases";
+import { createMessageObjectSchema } from "@/schemas/create-message-object";
+
+export const ZOD_ERROR_MESSAGES = {
+	REQUIRED: "Required",
+	EXPECTED_NUMBER: "Expected number, received nan",
+	NO_UPDATES: "No updates provided",
+};
+
+export const ZOD_ERROR_CODES = {
+	INVALID_UPDATES: "invalid_updates",
+};
+
+export const notFoundSchema = createMessageObjectSchema(
+	HttpStatusPhrases.NOT_FOUND,
+);
+
+export const unauthorizedSchema = createMessageObjectSchema(
+	HttpStatusPhrases.UNAUTHORIZED,
+);
+
+export const conflictSchema = createMessageObjectSchema(
+	HttpStatusPhrases.CONFLICT,
+);
+
+export const badRequestSchema = createMessageObjectSchema(
+	HttpStatusPhrases.BAD_REQUEST,
+);
