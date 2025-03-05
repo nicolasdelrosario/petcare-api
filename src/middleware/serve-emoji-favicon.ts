d6d1f059029b339e9b2fcd2300f97a00253ff288
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-03-04 20:07:30 -0500

    refactor: :recycle: add await

--- src/middleware/serve-emoji-favicon.ts
+++ src/middleware/serve-emoji-favicon.ts
@@ -8,6 +8,6 @@ export const serveEmojiFavicon = (emoji: string): MiddlewareHandler => {
 				`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" x="-0.1em" font-size="90">${emoji}</text></svg>`,
 			);
 		}
-		return next();
+		await next();
 	};
 };
