d4ca86c9366ab0bf9187f8f12df6f1c8b773d36a
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-03-05 16:55:30 -0500

    feat: :sparkles: add drizzle configuration

---
+++ drizzle.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from "drizzle-kit";
+
+// biome-ignore lint/style/noDefaultExport: Drizzle Kit needs a default export to properly load the config file.
+export default defineConfig({
+	dialect: "sqlite",
+	schema: "src/db/schemas",
+});
