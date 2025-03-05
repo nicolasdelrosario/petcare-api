26d2b4637de8c79c3b7c2288d6f574e99cd4c000
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-03-05 11:16:55 -0500

    feat: :sparkles: add json content one of helper

---
+++ src/helpers/json-content-one-of.ts
@@ -0,0 +1,19 @@
+import type { ZodSchema } from "@/helpers/types";
+
+import { oneOf } from "@/helpers/one-of";
+
+export const jsonContentOneOf = <T extends ZodSchema>(
+	schemas: T[],
+	description: string,
+) => {
+	return {
+		content: {
+			"application/json": {
+				schema: {
+					oneOf: oneOf(schemas),
+				},
+			},
+		},
+		description,
+	};
+};
