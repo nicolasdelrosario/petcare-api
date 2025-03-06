266358665208e1683190d1b3a99987b0cb23750d
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-03-06 17:18:25 -0500

    feat: :sparkles: add auth routes

---
+++ src/routes/auth/auth.routes.ts
@@ -0,0 +1,34 @@
+import { createRoute } from "@hono/zod-openapi";
+
+import * as HttpStatusCodes from "@/constants/http-status-codes";
+import { jsonContent } from "@/helpers/json-content";
+import { jsonContentRequired } from "@/helpers/json-content-required";
+import { badRequestSchema, unauthorizedSchema } from "@/lib/constants";
+import { loginResponseSchema, loginUserSchema } from "@/schemas/login-schema";
+
+const tags = ["Auth"];
+
+export const login = createRoute({
+	method: "post",
+	path: "/login",
+	tags,
+	request: {
+		body: jsonContentRequired(loginUserSchema, "The user credentials"),
+	},
+	responses: {
+		[HttpStatusCodes.OK]: jsonContent(
+			loginResponseSchema,
+			"The user credentials",
+		),
+		[HttpStatusCodes.UNAUTHORIZED]: jsonContent(
+			unauthorizedSchema,
+			"Invalid credentials",
+		),
+		[HttpStatusCodes.BAD_REQUEST]: jsonContent(
+			badRequestSchema,
+			"Invalid request structure or content",
+		),
+	},
+});
+
+export type LoginRoute = typeof login;
