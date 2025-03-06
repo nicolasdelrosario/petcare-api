a904861885d87515d974c2bcae03df4a280b77d6
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-03-06 17:14:48 -0500

    feat: :sparkles: add auth middleware

---
+++ src/middleware/auth.ts
@@ -0,0 +1,49 @@
+import * as HttpStatusCodes from "@/constants/http-status-codes";
+import * as HttpStatusPhrases from "@/constants/http-status-phrases";
+import type { Env } from "@/lib/types";
+import type { Context, Next } from "hono";
+import { getCookie } from "hono/cookie";
+import { verify } from "hono/jwt";
+
+export const authMiddleware = async (c: Context<Env>, next: Next) => {
+	const publicPaths = ["/", "/login", "/doc", "/reference"];
+
+	if (publicPaths.includes(c.req.path)) {
+		return next();
+	}
+
+	const authHeader = c.req.header("Authorization");
+	const cookieToken = getCookie(c, "token");
+
+	let token = cookieToken;
+	if (authHeader?.startsWith("Bearer ")) {
+		token = authHeader.substring(7);
+	}
+
+	if (!token) {
+		return c.json(
+			{ message: HttpStatusPhrases.UNAUTHORIZED },
+			HttpStatusCodes.UNAUTHORIZED,
+		);
+	}
+
+	try {
+		const payload = await verify(token, c.env.JWT_SECRET_KEY);
+
+		if (!payload) {
+			return c.json(
+				{ message: HttpStatusPhrases.UNAUTHORIZED },
+				HttpStatusCodes.UNAUTHORIZED,
+			);
+		}
+
+		c.set("jwtPayload", payload);
+
+		return next();
+	} catch {
+		return c.json(
+			{ message: HttpStatusPhrases.UNAUTHORIZED },
+			HttpStatusCodes.UNAUTHORIZED,
+		);
+	}
+};
