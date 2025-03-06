cb62ef0e31290b7da5f8f5085f1feff9a970a0cd
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-03-06 17:14:23 -0500

    feat: :label: update types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,13 +2,14 @@ import type { OpenAPIHono, RouteConfig, RouteHandler } from "@hono/zod-openapi";
 
 export type Bindings = {
 	DB: D1Database;
-	JWT_SECRET: string;
+	JWT_SECRET_KEY: string;
 	NODE_ENV: string;
+};
+
+export type Env = {
+	Bindings: Bindings;
 };
 
 export type AppOpenApi = OpenAPIHono<{ Bindings: Bindings }>;
 
-export type AppRouteHandler<R extends RouteConfig> = RouteHandler<
-	R,
-	{ Bindings: Bindings }
->;
+export type AppRouteHandler<R extends RouteConfig> = RouteHandler<R, Env>;
