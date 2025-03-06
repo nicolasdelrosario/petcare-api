7c43f85ba959e571c95ee3de0a42fd5ae5e0fe46
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-03-06 09:58:54 -0500

    feat: :sparkles: add user sanitization

---
+++ src/utils/user-sanitization.ts
@@ -0,0 +1,12 @@
+export const sanitizeUser = <T extends { password?: string }>(
+	user: T,
+): Omit<T, "password"> => {
+	const { password, ...safeUser } = user;
+	return safeUser;
+};
+
+export const sanitizeUsers = <T extends { password?: string }>(
+	users: T[],
+): Omit<T, "password">[] => {
+	return users.map(sanitizeUser);
+};
