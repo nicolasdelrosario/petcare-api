2224e5c14ee0fef602a2d94d3b559041f8b6a7a2
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-03-06 17:29:58 -0500

    feat: :sparkles: add cors and auth middleware to app

--- src/lib/create-app.ts
+++ src/lib/create-app.ts
@@ -1,15 +1,17 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { logger } from "hono/logger";
 
-import type { Bindings } from "@/lib/types";
+import type { Env } from "@/lib/types";
 
+import { authMiddleware } from "@/middleware/auth";
 import { defaultHook } from "@/middleware/default-hook";
 import { notFound } from "@/middleware/not-found";
 import { onError } from "@/middleware/on-error";
 import { serveEmojiFavicon } from "@/middleware/serve-emoji-favicon";
+import { cors } from "hono/cors";
 
 export function createRouter() {
-	return new OpenAPIHono<{ Bindings: Bindings }>({
+	return new OpenAPIHono<Env>({
 		strict: false,
 		defaultHook,
 	});
@@ -18,8 +20,16 @@ export function createRouter() {
 export function createApp() {
 	const app = createRouter();
 
+	app.use(
+		cors({
+			origin: "*",
+			credentials: true,
+		}),
+	);
+
 	app.use(serveEmojiFavicon("🐇"));
 	app.use(logger());
+	app.use(authMiddleware);
 
 	app.notFound(notFound);
 	app.onError(onError);
