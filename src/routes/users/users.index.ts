af726ae5e49bcae2827554213e739121990704ef
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-03-06 17:30:53 -0500

    feat: :sparkles: implement more routes to index

--- src/routes/users/users.index.ts
+++ src/routes/users/users.index.ts
@@ -5,4 +5,7 @@ import * as routes from "./users.routes";
 
 export const router = createRouter()
 	.openapi(routes.list, handlers.list)
-	.openapi(routes.create, handlers.create);
+	.openapi(routes.create, handlers.create)
+	.openapi(routes.getOne, handlers.getOne)
+	.openapi(routes.patch, handlers.patch)
+	.openapi(routes.remove, handlers.remove);
