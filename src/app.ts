dd3427f0aaea5945d3cb690a1c0ae25417b15131
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-03-06 17:30:17 -0500

    feat: :sparkles: import auth and users routes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
+import { router as auth } from "@/routes/auth/auth.index";
 import { router as index } from "@/routes/index.route";
+import { router as users } from "@/routes/users/users.index";
 import { configureOpenApi } from "./lib/configure-open-api";
 import { createApp } from "./lib/create-app";
 
 const app = createApp();
 
-const routes = [index] as const;
+const routes = [index, auth, users] as const;
 
 configureOpenApi(app);
 
