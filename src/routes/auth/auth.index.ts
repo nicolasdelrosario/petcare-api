84988153e209bd28cc85d5994853afb5f914043f
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-03-06 20:47:38 -0500

    feat: :sparkles: implement logout method

--- src/routes/auth/auth.index.ts
+++ src/routes/auth/auth.index.ts
@@ -3,4 +3,6 @@ import { createRouter } from "@/lib/create-app";
 import * as handlers from "./auth.handlers";
 import * as routes from "./auth.routes";
 
-export const router = createRouter().openapi(routes.login, handlers.login);
+export const router = createRouter()
+	.openapi(routes.login, handlers.login)
+	.openapi(routes.logout, handlers.logout);
