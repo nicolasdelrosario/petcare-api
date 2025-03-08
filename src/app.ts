5cf81cd034d00f8e09221277bac26500de7e63af
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-03-08 12:24:29 -0500

    feat: :sparkles: add workspaces routes to app

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import { router as auth } from "@/routes/auth/auth.index";
 import { router as index } from "@/routes/index.route";
 import { router as users } from "@/routes/users/users.index";
+import { router as workspaces } from "@/routes/workspaces/workspaces.index";
 import { configureOpenApi } from "./lib/configure-open-api";
 import { createApp } from "./lib/create-app";
 
 const app = createApp();
 
-const routes = [index, auth, users] as const;
+const routes = [index, auth, users, workspaces] as const;
 
 configureOpenApi(app);
 
